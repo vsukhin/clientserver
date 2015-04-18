c349a01da1474cde64179ea9cf3f5af595993220
vsukhin <vsukhin@virtuososoft.com>
2015-04-19 01:40:54 +0300

    Adding testing

--- client/tcpclient_test.go
+++ client/tcpclient_test.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestNewTcpClient(t *testing.T) {
+	var tcpport int = 1
+	var textdata = "text"
+
+	tcpclient := NewTcpClient(tcpport, textdata)
+	if tcpclient == nil {
+		t.Error("New tcp client should create a new object")
+	} else {
+		if tcpclient.tcpPort != tcpport {
+			t.Error("New tcp client should use a right tcp port value")
+		}
+		if tcpclient.textData != textdata {
+			t.Error("New tcp client should use a right text data")
+		}
+	}
+}
